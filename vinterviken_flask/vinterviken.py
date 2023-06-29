d75ea69e6af11e98dd49f8e977bf6d88db8191bd
Karl Ericsson <git@services.karlericsson.com>
2023-06-29 21:06:35 +0200

    Implement server side events

--- vinterviken_flask/vinterviken.py
+++ vinterviken_flask/vinterviken.py
@@ -1,11 +1,13 @@
 import jinja_partials
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 from .db import create_db
 from .models import db, Court
 from datetime import datetime
+import queue
 
 ALLOWED_ATTRIBUTES = {'booked', 'available', 'booked2h'}
-updated_time = datetime.now().replace(microsecond=0)
+updated_time = "Not updated since server restart."
+updated_queue = queue.Queue(maxsize=1)
 def create_app():
     app = Flask(__name__)
     jinja_partials.register_extensions(app)
@@ -16,14 +18,28 @@ def create_app():
     @app.route('/')
     def index():
         courts = db.session.execute(db.select(Court).order_by(Court.id)).scalars().all()
-        return render_template('index.html', courts=courts, updated_time=updated_time)
+        return render_template('index.html', courts=courts)
 
     @app.route('/toggle_status/<context>/<int:court_id>', methods=['POST'])
-    def toggle_status(context:str, court_id):    
+    def toggle_status(context:str, court_id):
+        global updated_time
+        updated_queue.put(datetime.now().replace(microsecond=0))
+        updated_time = datetime.now().replace(microsecond=0)
         court = Court.query.get(court_id)
         if context in ALLOWED_ATTRIBUTES and court:
             setattr(court, context, not getattr(court, context))
             db.session.commit()
         return render_template('partials/court.html', context=context, court=court)
+    
+    @app.route('/events')
+    def event():
+        def stream():
+            updated_queue.put(updated_time)
+            while True:
+                msg = f"data: <h4>Updated: {updated_queue.get()}</h4>\n\n"
+                yield msg
+
+        return Response(stream(), mimetype="text/event-stream")
+
 
     return app
